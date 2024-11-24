40e3fe60f0a13e6db9839382ce715ab6e0bfb056
Henrique Ambiel <henriqueambiel@gmail.com>
2024-11-24 13:20:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const posts = [
     {
         descricao: "Foto teste",
         imagem: "https://placecats.com/millie/300/150",
+        id: 1
     },
     {
         descricao: "Paisagem deslumbrante",
         imagem: "https://source.unsplash.com/random/300x200",
+        id: 2
     },
     {
         descricao: "Comida deliciosa",
         imagem: "https://picsum.photos/seed/picsum/300/200",
+        id: 3
     }
-];
-
+]
 // Cria uma instância do Express, que é o nosso servidor web
 const app = express();
 app.use(express.json());
@@ -29,4 +31,15 @@ app.listen(3000, () => {
 app.get("/posts", (req, res) => {
     // Define o status da resposta como 200 (OK) e envia a string "Teste" como corpo da resposta
     res.status(200).json(posts);
+});
+
+function buscarPosts(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    })
+};
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPosts(req.params.id)
+    res.status(200).json(posts[index]);
 });
