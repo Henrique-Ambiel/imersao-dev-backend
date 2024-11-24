466200d58a33bd29720c2982855a6b867165db6f
Henrique Ambiel <henriqueambiel@gmail.com>
2024-11-24 15:25:58 -0300

    Aula 3 - parte 2

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
 import express from 'express';
+import conectarAoBanco from './src/config/dbConfig.js';
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-const posts = [
-    {
-        descricao: "Foto teste",
-        imagem: "https://placecats.com/millie/300/150",
-        id: 1
-    },
-    {
-        descricao: "Paisagem deslumbrante",
-        imagem: "https://source.unsplash.com/random/300x200",
-        id: 2
-    },
-    {
-        descricao: "Comida deliciosa",
-        imagem: "https://picsum.photos/seed/picsum/300/200",
-        id: 3
-    }
-]
-// Cria uma instância do Express, que é o nosso servidor web
-const app = express();
-app.use(express.json());
+async function getTodosPosts(params) {
+    const db = conexao.db('imersao-dev');
+    const colecao = db.collection('posts');
+    return colecao.find().toArray();
+}
 
-// Inicia o servidor, escutando na porta 3000
-app.listen(3000, () => {
-    // Mensagem de log para indicar que o servidor está em funcionamento
-    console.log("Servidor escutando na porta 3000");
-});
-
-// Define uma rota GET para a URL '/api'
-app.get("/posts", (req, res) => {
-    // Define o status da resposta como 200 (OK) e envia a string "Teste" como corpo da resposta
+app.get("/posts", async (req, res) => {
+    const posts = await getTodosPosts();
     res.status(200).json(posts);
 });
 
-function buscarPosts(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    })
-};
+// function buscarPosts(id){
+//     return posts.findIndex((post) => {
+//         return post.id === Number(id);
+//     })
+// };
 
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPosts(req.params.id)
-    res.status(200).json(posts[index]);
-});
+// app.get("/posts/:id", (req, res) => {
+//     const index = buscarPosts(req.params.id)
+//     res.status(200).json(posts[index]);
+// });
